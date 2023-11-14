7a32cfc68adbd20d9391106186b07c6fd1895882
mrmtsu <81202muramatsu@gmail.com>
2023-11-15 07:34:27 +0900

    add marketing page

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,14 @@
+import { Footer } from "@/app/(marketing)/_components/footer";
+import { Navbar } from "@/app/(marketing)/_components/navbar";
+
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full bg-slate-100">
+      <Navbar />
+      <main className="pt-40 pb-20 bg-slate-100">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MarketingLayout;
