ae50288bf4f0d414c1c3ff1c12377ea866696bbf
mrmtsu <81202muramatsu@gmail.com>
2023-11-16 08:09:24 +0900

    add org

---
+++ app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -0,0 +1,11 @@
+import { OrganizationList } from "@clerk/nextjs";
+
+export default function CreateOrganaizationPage() {
+  return (
+    <OrganizationList
+      hidePersonal
+      afterSelectOrganizationUrl="/organization/:id"
+      afterCreateOrganizationUrl="/organization/:id"
+    />
+  );
+}
