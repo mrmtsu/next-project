54354fffa453a01ee84b59c02d11d1d2f43ebbce
mrmtsu <81202muramatsu@gmail.com>
2023-11-19 07:05:15 +0900

    add board popover form

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,19 +1,14 @@
-import { create } from "@/actions/create-board";
-import { Board } from "@/app/(platform)/(dashboard)/organization/[organizationId]/board";
-import { Form } from "@/app/(platform)/(dashboard)/organization/[organizationId]/form";
-import { Button } from "@/components/ui/button";
-import { db } from "@/lib/db";
+import { BoardList } from "@/app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list";
+import { Info } from "@/app/(platform)/(dashboard)/organization/[organizationId]/_components/info";
+import { Separator } from "@/components/ui/separator";
 
 const OrganizationIdPage = async () => {
-  const boards = await db.board.findMany();
-
   return (
-    <div>
-      <Form />
-      <div className="space-y-2">
-        {boards.map((board) => (
-          <Board key={board.id} title={board.title} id={board.id} />
-        ))}
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList />
       </div>
     </div>
   );
