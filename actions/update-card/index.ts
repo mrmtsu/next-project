4eb125e7283c7e4aa95a6424ca289ca1a4b8530d
mrmtsu <81202muramatsu@gmail.com>
2023-11-27 06:36:03 +0900

    add card mobal

---
+++ actions/update-card/index.ts
@@ -0,0 +1,46 @@
+"use server";
+
+import { UpdateCard } from "@/actions/update-card/schema";
+import { InputType, ReturnType } from "@/actions/update-card/types";
+import { createSafeAction } from "@/lib/create-safe-action";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { revalidatePath } from "next/cache";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  const { id, boardId, ...values } = data;
+  let card;
+
+  try {
+    card = await db.card.update({
+      where: {
+        id,
+        list: {
+          board: {
+            orgId,
+          },
+        },
+      },
+      data: {
+        ...values,
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Failed to update",
+    };
+  }
+
+  revalidatePath(`/board/${boardId}`);
+  return { data: card };
+};
+
+export const updateCard = createSafeAction(UpdateCard, handler);
